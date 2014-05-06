9fd7a5f545b1e07ccf8a54d101edd57360679482
rensdikkepens <r.vanschouwenburg@student.utwente.nl>
2014-05-06 16:05:49 +0200

    2.2 taak a
    practicum 05-06-2014

---
+++ session2/result2.sql
@@ -0,0 +1,3 @@
+INSERT into "F" VALUES ('ABC', 'EF', 'false', 'want ABC zijn de keys');
+INSERT into "F" VALUES ('E', 'D', 'true', 'een niet key attribuut is een determinant');
+INSERT into "F" VALUES ('D', 'A', 'true', 'een niet key attribuut is een determinant');
